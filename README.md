0b0461349ef9f2f396e5ddd7ca7f3d4a900b35ed
Ryan Radford <werkncode@gmail.com>
2020-05-06 10:39:31 -0700

    Remove extra PHP extensions, add Powershell setup script

--- README.md
+++ README.md
@@ -1,17 +1,36 @@
 # LAMP stack built with Docker Compose
 
-A basic LAMP stack environment built using Docker Compose. It consists of the following:
+This repository can be used to deploy LAMP stack apps.
 
-* PHP
-* Apache
-* MySQL
-* phpMyAdmin
+# Deploying Mac OS X Or Linux
 
-## Installation
+You can use either Docker Desktop or stand up your own Docker server with Docker + Docker Compose.
 
-```shell
-cp sample.env .env
-docker-compose up -d
-```
+Be sure to update `.env` with the proper credentials and names
+for your database users, database name, and database passwords.
 
-Your LAMP stack is now ready!! You can access it via `http://localhost`.
+**Deployment**:
+
+`bash setup-webapp.php http://github.com/your-repo.git`
+
+**Re-deploy over your existing app (this will destory everything)**:
+
+`bash setup-webapp.php http://github.com/your-repo.git --nuke`
+
+**Erase everything (this will destroy everything)**:
+
+`sudo bash remove-webapp.php`
+
+# Windows Support:
+
+Note, `git-bash` and other Windows bash apps will not work with this setup script due to path expansion, see here for more details [https://github.com/git-for-windows/git/issues/577](https://github.com/git-for-windows/git/issues/577).  You can deploy this using `WSL Ubuntu`, `Virtual Machine`, etc... just not using `git-bash` for Windows.
+
+# App Settings
+
+You can place a script called `install.sh` in the root of your app repository you are deploying to have that script ran within your container after deployment.
+
+This is a good place to do post install configuration like populating a database or setting permissions on folders.
+
+# If you find your missing a PHP package or App
+
+Look in `./bin/webserver/Dockerfile` and add packages or other features you want there.
---
+++ setup-webapp.ps1
@@ -0,0 +1,84 @@
+param (
+    [string]$nuke = "false",
+    [Parameter(Mandatory=$true)][string]$gitHubRepo
+ )
+
+Write-Host $nuke
+Write-Host $gitHubRepo
+
+#install our git repo, where $1 is the path to GitHub repo to install
+
+#if nuke option is set, blow away all containers
+#erase existing installation
+
+<#
+if [[ "$2" == "--nuke" ]];
+then
+	echo "Nuking current installation..."
+	sudo docker rm -f $(sudo docker ps -a -q);
+	rm -Rf data
+	rm -Rf logs
+	rm -Rf www
+fi
+
+#setup folders, clone repo
+mkdir data ; mkdir data/mysql
+mkdir logs ; mkdir logs/mysql; mkdir logs/apache2; mkdir logs/php
+mkdir www ; cd www
+
+#clone repo
+git clone $1
+
+#get our folder name
+repoFolderName=$(basename ${1})
+
+#change into our www dir
+#all dockerized apps use branch docker-setup,
+#check that branch out
+cd ${repoFolderName}
+git checkout docker-setup
+
+#dump the readme to the console if it exists in the repo specifically we use an html comment
+#to communicate post config operations when using docker
+#ie: somewhere in your readme add #docker-post-install-msg: YOUR MESSAGE HERE, URL, ETC...
+if [[ -e README.md ]];
+then
+	echo "Dumping README.md docker-post-install-msg for repo ($1)"
+	cat README.md | grep 'docker-post-install-msg' | sed -n "s/^.*'\(.*\)'.*$/\1/ p"
+fi
+
+cd ../..
+
+echo "Bringup up LAMP stack + installed web app for ($1)"
+
+pwd
+
+# set environment variables for export
+set -a
+# export secrets from .env (access within docker-compose using environment: -VARIABLENAME)
+source .env
+
+# bring up docker containers + app
+sudo docker-compose up -d
+
+# make sure container is running before proceeding
+echo "Waiting for docker containers to come up before proceeding..."
+sleep 20s
+
+#if www/appname has a install.sh, run it it contains post config scripts
+if [[ -e ./www/${repoFolderName}/install.sh ]];
+then
+	echo "Running post install script for app: $(basename ${1})"
+	sudo docker exec -it werkn-apache-webserver bash /var/www/html/${repoFolderName}/install.sh
+fi
+
+#make sure our web app is owned by www-data (fix for upload folder permissions)
+sudo docker cp ./bin/webserver/apache2/configure-permissions.sh werkn-apache-webserver:/etc/apache2/
+
+#copy into web server config script for .htpasswd
+sudo docker cp ./bin/webserver/apache2/build-sites-enabled-confs.sh werkn-apache-webserver:/etc/apache2/
+
+#run script in interactive shell
+sudo docker exec -it werkn-apache-webserver bash /etc/apache2/configure-permissions.sh
+sudo docker exec -it werkn-apache-webserver bash /etc/apache2/build-sites-enabled-confs.sh
+#>
